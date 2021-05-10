b80fe9ab4ddd179a22c17039233539c0e6140bf0
Yu Hai <2831214821@qq.com>
2021-05-10 16:29:15 +0800

    add tcp-a

---
+++ socket/fileTcpClient.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+
+func main() {
+	file := os.Args[1]
+	host := os.Args[2]
+
+	address := host
+	raddr, err := net.ResolveUDPAddr("udp", address)
+	print(raddr)
+	if err != nil {
+		return
+	}
+
+	//conn, err := net.DialUDP("udp", nil, raddr)
+
+	if err != nil {
+		return
+	}
+
+	//defer conn.Close()
+
+	pfile, e := os.Open(file)
+	if e != nil {
+		fmt.Printf("open file %s error: %s\n", file, e.Error())
+	}
+	reader := bufio.NewReader(pfile)
+
+	var buffer = make([]byte, 2000)
+
+	connu, err := net.DialUDP("udp", nil, raddr)
+
+	for {
+		nRead, err := io.ReadFull(reader, buffer)
+		if err != nil {
+			return
+		}
+		fmt.Printf("read %d bytes\n", nRead)
+
+		//n, err := io.CopyBuffer(conn, reader, buffer)
+		//if err != nil {
+		//	fmt.Printf("Err: %s\n", err.Error())
+		//	return
+		//}
+		//write, err := conn.Write(buffer)
+		//breader := bytes.NewReader(buffer)
+		//print(host, " ", err)
+
+		//write, err := bufio.NewReader(connu).Read(buffer)
+		writer := bufio.NewWriter(connu)
+		write, err := writer.Write(buffer)
+		writer.Flush()
+
+		if err != nil {
+			print("Write err: ", err)
+			return
+		}
+		fmt.Printf("packet-written: bytes=%d\n", write)
+	}
+}
---
+++ socket/fileTcpServer.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func FileServerStart(address string, file *os.File) (err error) {
+	var maxBufferSize = 1024000
+
+	addr := net.UDPAddr{
+		Port: 1900,
+		IP: net.ParseIP("0.0.0.0"),
+	}
+	//pc, err := net.ListenUDP("udp", address)
+	pc, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		return
+	}
+	fmt.Printf("%s\n", pc.LocalAddr().String())
+
+	buffer := make([]byte, maxBufferSize)
+
+	for {
+		n, addr, _:= pc.ReadFrom(buffer)
+		_, err := file.Write(buffer)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("packet-received: bytes=%d from=%s\n",
+			n, addr.String())
+	}
+}
+
+func main() {
+	file := os.Args[1]
+	pfile, err := os.Create(file)
+	if err != nil {
+		fmt.Printf("open file %s error: %s\n", file, err.Error())
+	}
+	err = FileServerStart("0.0.0.0:1900", pfile)
+	if err != nil {
+		return
+	}
+}
